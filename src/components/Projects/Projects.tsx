d10dcee2adeee7ffde816da995e5f9cf5b60700d
Kenny <kenny136@outlook.com.au>
2023-09-04 21:37:18 +1000

    project data

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,14 +1,25 @@
 import styles from "./Projects.module.scss";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
-const Projects = () => {
-	return (
-		<div className={styles.wrapper}>
-			<NavBar />
-			<main>Content</main>
-			<Footer />
-		</div>
-	);
-};
+import { projects } from "../../static/data";
+
+const Projects = () => (
+	<div className={styles.wrapper}>
+		<NavBar />
+		<main>
+			{projects.map((project) => {
+				return (
+					<h1>
+						{project.name}
+						<p>{project.summary}</p>
+						{project.tools}
+						<a href={project.gitHub}>GitHub Link</a>
+					</h1>
+				);
+			})}
+		</main>
+		<Footer />
+	</div>
+);
 
 export default Projects;
---
+++ src/static/data.ts
@@ -0,0 +1,40 @@
+export const projects = [
+	{
+		name: "FakeOS",
+		summary:
+			"A Fake 'Desktop OS' Copy created with HTML, SCSS & JavaScript. Currently Has 3 working application Inside of it (Calculator, Weather App, DadJokes App.) ",
+		tools: ["HTML", "SCSS", "JavaScript"],
+		gitHub: "https://github.com/Kenny-136/FakeOS",
+		live: "https://kenny-136.github.io/FakeOS/",
+	},
+	{
+		name: "Morse Translator",
+		summary:
+			"an application that translates words / sentences created with English alphabets into Morse Code and vice-versa",
+		tools: ["HTML", "SCSS", "JavaScript", "Jest"],
+		gitHub: "https://github.com/Kenny-136/morse-translator",
+		live: "https://kenny-136.github.io/morse-translator/",
+	},
+	{
+		name: "Google Books",
+		summary:
+			"This project is created using React & Material UI Framework Library where users can search for books in the Google Books database. The page includes a search form, and when users submit a query, it fetches book data using async/await from the Google Books API and displays the results in a grid with book images, authors, titles, and descriptions",
+		tools: ["JavaScript", "React", "Material UI"],
+		gitHub: "https://github.com/Kenny-136/e-commerce",
+		live: "https://kenny-ggle-books.netlify.app/",
+	},
+	{
+		name: "E-Commerce",
+		summary:
+			"This project is a hands-on exercise in building a React-based e-commerce website that demonstrates my ability to fetch data, manage routes, integrate Firestore for data storage, and  implement a shopping cart system functionality.",
+		tools: ["React", "Firebase", "SCSS"],
+		gitHub: "https://github.com/Kenny-136/e-commerce",
+	},
+	{
+		name: "Minesweeper",
+		summary:
+			"Terminal based popular minesweeper game created in Java, Running in terminal with configurable Grid & Mines..",
+		tools: ["Java"],
+		gitHub: "https://github.com/Kenny-136/mnswpr",
+	},
+];
